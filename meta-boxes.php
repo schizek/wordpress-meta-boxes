bf868b932da11c49622eaebd1f589446b044c47d
schizek <schizek@dlsstudios.com>
2013-09-01 21:17:46 -0500

    Remove example from comments
    It's now in the readme

--- meta-boxes.php
+++ meta-boxes.php
@@ -4,28 +4,6 @@
  *
  * A class to easily add custom meta boxes and fields programmatically
  *
- * // Example Usage
- * $meta_box = array(
- *     'id'        => 'my-custom-meta',
- *     'title'     => 'My Metabox Title',
- *     'post_type' => 'page',
- *     'context'   => 'normal',
- *     'priority'  => 'default',
- *     'fields'    => array(
- *         array(
- *             'label' => 'Description',
- *             'key'   => 'my-custom-desc',
- *             'type'  => 'textarea',
- *         ),
- *         array(
- *             'label' => 'Goal Amount',
- *             'key'   => 'my-custom-amount',
- *             'type'  => 'text',
- *         ),
- *     ),
- * );
- * $meta_box_setup = new FD_Meta_Boxes($meta_box);
- *
  * @version 0.1
  * @author Fetch Designs
  * @copyright 2013 Fetch Designs <http://www.fetchdesigns.com/>
